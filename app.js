2ef8c220ce41282a05a6449c1b5d615b5ef2e750
Nery Santizo <santizo707@gmail.com>
2022-09-21 16:45:08 -0700

    price alert

--- app.js
+++ app.js
@@ -12,23 +12,38 @@ fetch("https://fakestoreapi.com/products/")
     for (let i = 0; i < data.length; i++) {
       const curData = data[i]
       html += `
-    <div class ="shop">
-      <h1 class="title">${curData.title}</h1>
-      <img src=${curData.image} alt="img" class="images">
-      <p class="description">${curData.description}</p>
-      <p class="price">${curData.price}</p>
+      <div class ="shop">
+        <h1 class="title">${curData.title}</h1>
+        <img src=${curData.image} alt="img" class="images">
+        <p class="description">${curData.description}</p>
+        <p class="price">${curData.price}</p>
       </div>`
     }
     shop.innerHTML = html
+
+    // add a click event to all titles
+    const titles = document.getElementsByClassName("title")
+    for (let i = 0; i < titles.length; i++) {
+      const showTitle = titles[i]
+      showTitle.addEventListener("click", (evt) => {
+        console.log(evt.target.innerHTML)
+      })
+    }
+
     const collection = document.getElementsByClassName("price")
     console.log(collection)
+
+    // for every element in the collection -> add an event listener
+
+    for (let i = 0; i < collection.length; i++) {
+      const showPrice = collection[i]
+      showPrice.addEventListener("click", (evt) => {
+        console.log(evt.target.innerHTML)
+      })
+    }
 
     // TODO: when a user clicks on a price - it should trigger an alert that reads 'The price of this item is ${}'
   })
   .catch((err) => {
     console.log(err)
   })
-function alertMessage() {
-  const alert = document.getElementById("price").value
-  alert(`The price of this item is + ${price}`)
-}
